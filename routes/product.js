0576297cefb0e6ed0a227841367210153ed21f98
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-09-04 13:37:29 +0700

    change connection

--- routes/product.js
+++ routes/product.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
 
 module.exports = {
-    getProd: (req, res) =>{
+    getProd: (req, res) => {
         let sql = `select * from products`;
         con.query(sql, (err, result) => {
             var products = [];
-            if (err) throw err;
-            for (var i = 0; i < result.length; i++) {
-                products.push({
-                    id: result[i].id,
-                    avatar: result[i].avatar,
-                    name: result[i].name,
-                    price: result[i].price,
-                    soLuongTon: result[i].soLuongTon,
-                    description: result[i].description
-                });
+            if (err) {
+                throw console.log(JSON.stringify(err));
+                res.json({"Error": 1, "Message": "Error while getting the data from Remote DataBase motherofall.org"});
+            } else {
+                for (var i = 0; i < result.length; i++) {
+                    products.push({
+                        id: result[i].id,
+                        avatar: result[i].avatar,
+                        name: result[i].name,
+                        price: result[i].price,
+                        soLuongTon: result[i].soLuongTon,
+                        description: result[i].description
+                    });
+                }
+                res.send(JSON.stringify(products));
             }
-            res.send(JSON.stringify(products));
         })
     },
-    insertProd:function (req,res,next){
-        var avatar=req.body.avatar;
+    insertProd: function (req, res, next) {
+        var avatar = req.body.avatar;
         var name = req.body.name;
         var price = req.body.price;
         var soLuongTon = req.body.soLuongTon;
-        var description=req.body.description;
+        var description = req.body.description;
 
         var sql = `INSERT INTO products(avatar,name,price,soLuongTon,description) VALUES("${avatar}","${name}","${price}","${soLuongTon}","${description}")`;
 
@@ -33,20 +37,20 @@ module.exports = {
             res.json({'status': 'succes', id: result.insertId});
         })
     },
-    updateProd:function (req,res,next){
-        var sql ="UPDATE products SET price='"+req.body.price+"',soLuongTon='"+req.body.soLuongTon+"',description='"+req.body.description+"' WHERE id="+req.body.id+"";
+    updateProd: function (req, res, next) {
+        var sql = "UPDATE products SET price='" + req.body.price + "',soLuongTon='" + req.body.soLuongTon + "',description='" + req.body.description + "' WHERE id=" + req.body.id + "";
 
-        con.query(sql,function (err, result,fields){
+        con.query(sql, function (err, result, fields) {
             if (err) throw err;
-            res.json({status:"Updated successfully"});
+            res.json({status: "Updated successfully"});
         })
     },
-    deleteProd:function (req,res,next){
-        var sql ="DELETE FROM products WHERE id="+req.body.id+"";
+    deleteProd: function (req, res, next) {
+        var sql = "DELETE FROM products WHERE id=" + req.body.id + "";
 
-        con.query(sql,function (err, result,fields){
+        con.query(sql, function (err, result, fields) {
             if (err) throw err;
-            res.json({status:"Deleted successfully"});
+            res.json({status: "Deleted successfully"});
         })
     }
 };
