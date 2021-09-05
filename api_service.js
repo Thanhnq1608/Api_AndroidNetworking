27eab65df92c178c76878a897ef4cd892bb5282e
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-09-05 16:52:13 +0700

    ok

--- api_service.js
+++ api_service.js
@@ -11,69 +11,62 @@ var config = ({
     host: '37.59.55.185',
     user: 'YPA5lop9VD',
     password: 'CoEo4yDqo6',
-    port: process.env.PORT ||8080,
+    port: process.env.PORT || 8080,
     database: 'YPA5lop9VD',
 });
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+var con = mysql.createConnection(config);
+global.con = con;
+con.connect(function (err) {
+    if (err) {
+        console.error('CONNECT FAILED', err.code);
+    } else {
+        console.log("DB connection succeded.");
 
-function startConnection() {
-    console.error('CONNECTING');
-    var con = mysql.createConnection(config);
-    global.con=con;
-    con.connect(function (err) {
-        if (err) {
-            console.error('CONNECT FAILED', err.code);
-            startConnection();
-        } else
-            console.log("DB connection succeded.");
-    });
-    con.on('error', function (err) {
-        if (err.fatal)
-            startConnection();
-    });
-}
+    }
 
-startConnection();
-//
 // app.get("/", (req, res) => {
 //     res.send("Hello world");
 // })
-app.get( '/',(req, res) => {
-    let sql = `select * from products`;
-    con.query(sql, (err, result) => {
-        var products = [];
-        if (err) {
-            console.log(JSON.stringify(err));
-            res.json({"Error": 1, "Message": "Error while getting the data from Remote DataBase motherofall.org"+err});
-        } else {
-            for (var i = 0; i < result.length; i++) {
-                products.push({
-                    id: result[i].id,
-                    avatar: result[i].avatar,
-                    name: result[i].name,
-                    price: result[i].price,
-                    soLuongTon: result[i].soLuongTon,
-                    description: result[i].description
+    app.get('/', (req, res) => {
+        let sql = `select * from products`;
+        con.query(sql, (err, result) => {
+            var products = [];
+            if (err) {
+                console.log(JSON.stringify(err));
+                res.json({
+                    "Error": 1,
+                    "Message": "Error while getting the data from Remote DataBase motherofall.org" + err
                 });
+            } else {
+                for (var i = 0; i < result.length; i++) {
+                    products.push({
+                        id: result[i].id,
+                        avatar: result[i].avatar,
+                        name: result[i].name,
+                        price: result[i].price,
+                        soLuongTon: result[i].soLuongTon,
+                        description: result[i].description
+                    });
+                }
+                res.send(JSON.stringify(products));
             }
-            res.send(JSON.stringify(products));
-        }
-    })
-},)
+        })
+    },)
 // CRUD Product
-app.post('/insertProd', insertProd);
-app.put('/updateProd', updateProd);
-app.post('/deleteProd', deleteProd);
+    app.post('/insertProd', insertProd);
+    app.put('/updateProd', updateProd);
+    app.post('/deleteProd', deleteProd);
 // CRUD User
-app.get('/getUser', getUser);
-app.post('/insertUser', insertUser);
-app.put('/updateUser', updateUser);
-app.post('/changePass', changePass);
-app.post('/deleteUser', deleteUser);
+    app.get('/getUser', getUser);
+    app.post('/insertUser', insertUser);
+    app.put('/updateUser', updateUser);
+    app.post('/changePass', changePass);
+    app.post('/deleteUser', deleteUser);
 
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log('Server started on port 3000..');
-});
+    app.listen(process.env.PORT || 8080, () => {
+        console.log('Server started on port 3000..');
+    });
