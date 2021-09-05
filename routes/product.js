4422ec644610bde5dbd51afbe1c33274509ee02e
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-09-05 16:43:32 +0700

    ok

--- routes/product.js
+++ routes/product.js
@@ -1,28 +1,7 @@
 const fs = require('fs');
 
 module.exports = {
-    getProd: (req, res) => {
-        let sql = `select * from products`;
-        con.query(sql, (err, result) => {
-            var products = [];
-            if (err) {
-                  console.log(JSON.stringify(err));
-                res.json({"Error": 1, "Message": "Error while getting the data from Remote DataBase motherofall.org"+err});
-            } else {
-                for (var i = 0; i < result.length; i++) {
-                    products.push({
-                        id: result[i].id,
-                        avatar: result[i].avatar,
-                        name: result[i].name,
-                        price: result[i].price,
-                        soLuongTon: result[i].soLuongTon,
-                        description: result[i].description
-                    });
-                }
-                res.send(JSON.stringify(products));
-            }
-        })
-    },
+
     insertProd: function (req, res, next) {
         var avatar = req.body.avatar;
         var name = req.body.name;
