24b8a5ad5c42c9a12271e2370bd324686a02cab6
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-10-02 01:04:49 +0700

    update decentralization

--- api_service.js
+++ api_service.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
 var app = express();
-var port = process.env.PORT ||3000 ;
+var port = process.env.PORT ;
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
 // const dbConfig = require('./database/db.config');
